047186d7c07bb6c6a0d1975a01f145f3e47e61ce
Carlos Ramirez <cramirezmun@unal.edu.co>
2024-04-17 09:35:57 -0500

    Add Tailwind CSS configuration and update App component in view/src

--- view/src/App.jsx
+++ view/src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  return <p>hi</p>;
+  const [todos, setTodos] = useState([]);
+  return (
+    <>
+      <h2 className="text-3xl font-bold">To-do List</h2>
+    </>
+  );
 }
 
 export default App;
